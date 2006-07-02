efa487c8fc0ef8aa1fb7be14f59f27d41ec75cf5
jhiller <jhiller@411c844f-e91d-0410-aa1e-e186e184c3e4>
2006-07-02 20:09:19 +0000

    Tagged for 0.1.1

---
+++ org.junitext/tags/0.1.1/src/java/org/junitext/runners/AnnotationRunner.java
@@ -0,0 +1,15 @@
+package org.junitext.runners;
+
+import org.junit.internal.runners.InitializationError;
+import org.junit.internal.runners.TestClassRunner;
+import org.junitext.internal.runners.AnnotationHandlingTestClassMethodsRunner;
+
+/**
+ * Test runner which supports annotation handling.
+ */
+public class AnnotationRunner extends TestClassRunner {
+
+	public AnnotationRunner(Class<?> clazz) throws InitializationError {
+		super(clazz, new AnnotationHandlingTestClassMethodsRunner(clazz));
+	}
+}
