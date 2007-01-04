e28aa67b9b72372d4b9482c2e5f170d3a7638ac6
jhiller <jhiller@411c844f-e91d-0410-aa1e-e186e184c3e4>
2007-01-04 09:45:08 +0000

    Tagged for version 0.2.1

---
+++ org.junitext/tags/0.2.1/src/java/org/junitext/runner/AnnotationHandlingJUnit4TestAdapter.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2006-2007, Jochen Hiller.
+ */
+package org.junitext.runner;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.JUnit4TestAdapterCache;
+import junit.framework.TestResult;
+
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+
+/**
+ * JUnit4Adapter which supports annotation handling.
+ * 
+ * We have to create our own runner based on an annotation request. As runner in
+ * superclass is private, we have to override all methods, referring fRunner.
+ * 
+ * TODO: ugly, cleanup with JUnit
+ */
+public class AnnotationHandlingJUnit4TestAdapter extends JUnit4TestAdapter {
+
+	Runner ourRunner;
+
+	JUnit4TestAdapterCache ourCache;
+
+	public AnnotationHandlingJUnit4TestAdapter(Class<?> newTestClass) {
+		this(newTestClass, JUnit4TestAdapterCache.getDefault());
+	}
+
+	public AnnotationHandlingJUnit4TestAdapter(final Class<?> newTestClass,
+			JUnit4TestAdapterCache cache) {
+		super(newTestClass, cache);
+		ourCache = cache;
+		ourRunner = AnnotationHandlingRequest.aClass(newTestClass).getRunner();
+	}
+
+	public int countTestCases() {
+		return ourRunner.testCount();
+	}
+
+	public void run(TestResult result) {
+		ourRunner.run(ourCache.getNotifier(result, this));
+	}
+
+	public Description getDescription() {
+		return ourRunner.getDescription();
+	}
+}
---
+++ org.junitext/tags/0.2.1/src/test/org/junitext/manipulation/PrerequisiteFilterTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2006-2007, Jochen Hiller.
+ */
+package org.junitext.manipulation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.internal.runners.InitializationError;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+import org.junitext.Prerequisite;
+
+/**
+ * Tests for the {@link PrerequisiteFilter} for JUnitExt.
+ */
+public class PrerequisiteFilterTest {
+
+	public static class APrereqTest {
+		@Test public void valid() {}
+		@Prerequisite(requires = "isTrue") @Test public void willBeRun1() {}
+		@Prerequisite(requires = "isFalse") @Test public void willNotBeRun1() {}
+		@Prerequisite(requires = "isTrue", callee = APrereqTestHelper.class) @Test public void willBeRunViaHelper1() {}
+		@Prerequisite(requires = "isFalse", callee = APrereqTestHelper.class) @Test public void willNotBeRunViaHelper1() {}
+		public boolean isTrue() { return true; }
+ 		public boolean isFalse() { return false; }
+	}
+	
+	public static class APrereqTestHelper {
+		public static boolean isTrue() { return true; }
+ 		public static boolean isFalse() { return false; }
+	}
+
+	@Ignore ("TODO: does not work. Requires access to test instance object. Clarify with Kent Beck")
+	@Test public void callWithFilter() throws InitializationError {
+		JUnitCore core = new JUnitCore();
+		Request req = Request.aClass(APrereqTest.class);
+		Result result = core.run(req.filterWith(new PrerequisiteFilter()));
+		// TODO: current result: 2, which is wrong, as instance call to isFalse() fails 
+		assertEquals(1+2, result.getRunCount()); 
+		assertEquals(0, result.getFailureCount());
+		// TODO: current result: 0, which is wrong, as filter can not raise a failure
+		assertEquals(2, result.getIgnoreCount());		
+	}
+
+	public static void main(String... args) {
+		JUnitCore.main(PrerequisiteFilterTest.class.getName());
+	}
+}
