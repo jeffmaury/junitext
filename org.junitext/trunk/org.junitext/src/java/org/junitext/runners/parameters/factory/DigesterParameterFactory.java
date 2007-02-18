796de1ed69c2881fa4ea1dc7ea5c98ce9818c7d0
jedijim <jedijim@411c844f-e91d-0410-aa1e-e186e184c3e4>
2007-02-18 14:50:15 +0000

    Refactored BasicDigesterParameterFactoryTest for better maintainability

--- org.junitext/trunk/org.junitext/src/java/org/junitext/runners/parameters/factory/DigesterParameterFactory.java
+++ org.junitext/trunk/org.junitext/src/java/org/junitext/runners/parameters/factory/DigesterParameterFactory.java
@@ -20,7 +20,7 @@ import java.util.List;
 import org.apache.commons.digester.Digester;
 
 /**
- * Creates sets of test parameters by parsing input XML using the Jakarta 
+ * Creates sets of test parameters by parsing input XML using the Jakarta
  * commons-digester.
  * 
  * @author Jim Hurne
@@ -33,21 +33,23 @@ public class DigesterParameterFactory implements ParameterFactory {
 	 */
 	public List<Object[]> createParameters(Class<?> testClass, File xmlFile)
 			throws Exception {
-		
+
 		InputStream xmlInput = new FileInputStream(xmlFile);
 		return createParameters(xmlInput);
 	}
-	
+
 	/**
-	 * Creates sets of test parameters by parsing input XML using the Jakarta 
+	 * Creates sets of test parameters by parsing input XML using the Jakarta
 	 * commons-digester.
 	 * 
-	 * @param xmlInput an <code>InputStream</code> containing the XML to parse
+	 * @param xmlInput
+	 *            an <code>InputStream</code> containing the XML to parse
 	 * @return the sets of test parameters
 	 * @throws Exception
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Object[]> createParameters(InputStream xmlInput) throws Exception {
+	public List<Object[]> createParameters(InputStream xmlInput)
+			throws Exception {
 		// Step 1 - Create the digester
 		Digester digester = new Digester();
 		digester.setValidating(false);
@@ -57,10 +59,10 @@ public class DigesterParameterFactory implements ParameterFactory {
 		registerBaseRules(digester);
 		registerBasicObjectRules(digester);
 		registerCollectionRules(digester);
-		
 
 		// Step 3 - Run the digester against the given input file
-		List<List<?>> generatedParmSets = (List<List<?>>) digester.parse(xmlInput);
+		List<List<?>> generatedParmSets = (List<List<?>>) digester
+				.parse(xmlInput);
 
 		// Step 4- Convert our list of lists to a list of object arrays
 		List<Object[]> returnParmSet = new ArrayList<Object[]>();
@@ -68,9 +70,9 @@ public class DigesterParameterFactory implements ParameterFactory {
 			returnParmSet.add(parmSet.toArray());
 		}
 
-		return returnParmSet;		
+		return returnParmSet;
 	}
-	
+
 	private void registerBaseRules(Digester digester) {
 		// Add a list for the top level <tests> element
 		digester.addObjectCreate("tests", java.util.ArrayList.class);
@@ -80,66 +82,74 @@ public class DigesterParameterFactory implements ParameterFactory {
 
 		// Add the <test> list to the top level <tests> list
 		digester.addSetNext("tests/test", "add");
-		
+
 		// Create an object for any "beans" that are encountered
 		// We use the wildcard so that any bean tag at any nesting level is
 		// handled
 		digester.addObjectCreate("*/bean", "java.lang.Object", "class");
 
-		// Set the properties on the beans based on the 
+		// Set the properties on the beans based on the
 		// <property name="" value="" > tag
 		digester.addSetProperty("*/bean/property", "name", "value");
 
 		// Add the bean to the parameter set
-		digester.addSetNext("*/bean", "add");		
+		digester.addSetNext("*/bean", "add");
 	}
-	
+
 	private void registerBasicObjectRules(Digester digester) {
-		//All of the basic object rules work the same.  If we encounter
-		//a basic object, then record the "value" of the basic object
-		//as a parameter that is used to call the add method on the current
-		//<test> list.
-		
-		//-- Rules for Strings
-		digester.addCallMethod("tests/test/string", "add", 1, new Class[] { String.class });
+		// All of the basic object rules work the same. If we encounter
+		// a basic object, then record the "value" of the basic object
+		// as a parameter that is used to call the add method on the current
+		// <test> list.
+
+		// -- Rules for Strings
+		digester.addCallMethod("tests/test/string", "add", 1,
+				new Class[] { String.class });
 		digester.addCallParam("tests/test/string", 0, "value");
-		
-		//-- Rules for Integers
-		digester.addCallMethod("tests/test/int", "add", 1, new Class[] { Integer.class });
+
+		// -- Rules for Integers
+		digester.addCallMethod("tests/test/int", "add", 1,
+				new Class[] { Integer.class });
 		digester.addCallParam("tests/test/int", 0, "value");
 
-		//-- Rules for the Shorts
-		digester.addCallMethod("tests/test/short", "add", 1, new Class[] { Short.class });
+		// -- Rules for the Shorts
+		digester.addCallMethod("tests/test/short", "add", 1,
+				new Class[] { Short.class });
 		digester.addCallParam("tests/test/short", 0, "value");
 
-		//-- Rules for the Longs
-		digester.addCallMethod("tests/test/long", "add", 1, new Class[] { Long.class });
+		// -- Rules for the Longs
+		digester.addCallMethod("tests/test/long", "add", 1,
+				new Class[] { Long.class });
 		digester.addCallParam("tests/test/long", 0, "value");
-		
-		//-- Rules for the Booleans
-		digester.addCallMethod("tests/test/boolean", "add", 1, new Class[] { Boolean.class });
-		digester.addCallParam("tests/test/boolean", 0, "value");	
+
+		// -- Rules for the Booleans
+		digester.addCallMethod("tests/test/boolean", "add", 1,
+				new Class[] { Boolean.class });
+		digester.addCallParam("tests/test/boolean", 0, "value");
 	}
-	
+
 	private void registerCollectionRules(Digester digester) {
-		//-- Rules for List Processing
+		// -- Rules for List Processing
 		// Create an ArrayList for each <list> element
 		digester.addObjectCreate("*/list", ArrayList.class);
-		digester.addObjectCreate("*/list/list", ArrayList.class);
-
-		// If the list is nested within another list, add it to the parent list
-		digester.addSetNext("*/list/list", "add");
 		
-		// Add the object (whatever it happens to be) set previously with 
+		// Add the newly created ArrayLists as a parameter so that it can be
+		// added to the properties of the parent bean (or added to another
+		// collection)
+		digester.addCallParam("*/list", 0, true);		
+		
+		// Add the list set previously with
 		// the CallParamRule to the bean property
 		SetPropertyWithParameterRule setPropertyWithObject = new SetPropertyWithParameterRule(
 				"name");
 		digester.addRule("*/bean/property", setPropertyWithObject);
-		
-		// Add the newly created ArrayLists as a parameter so that it can be added
-		// to the properties of the parent bean (or added to another collection)
-		digester.addCallParam("*/list", 0, true);
-		
+
+		// -- Rules for lists nested under lists
+		// Create an ArrayList for each nested <list> element
+		digester.addObjectCreate("*/list/list", ArrayList.class);
+
+		// If the list is nested within another list, add it to the parent list
+		digester.addSetNext("*/list/list", "add");
 	}
 
 }
--- org.junitext/trunk/org.junitext/src/test/org/junitext/runners/parameters/factory/BasicDigesterParameterFactoryTest.java
+++ org.junitext/trunk/org.junitext/src/test/org/junitext/runners/parameters/factory/BasicDigesterParameterFactoryTest.java
@@ -12,7 +12,6 @@
 package org.junitext.runners.parameters.factory;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -26,171 +25,110 @@ import org.junitext.runners.Robot;
 public class BasicDigesterParameterFactoryTest {
 
 	private DigesterParameterFactory testFactory;
-	
+
 	@Before
 	public void createTestFactory() {
 		testFactory = new DigesterParameterFactory();
 	}
-	
+
 	@Test
 	public void parseString() throws Exception {
 		String expectedString = "This is the expected string value.";
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
-			 + "<tests>"
-			 + "<test>"
-			 + "<string id=\"testString\" value=\""+ expectedString + "\" />"
-			 + "</test>"
-			 + "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
-		//Run the test
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
-		
-		//Verify the correct object structure was created
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+				+ "<tests>" + "<test>" + "<string id=\"testString\" value=\""
+				+ expectedString + "\" />" + "</test>" + "</tests>";
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of objects were not created.", 1,
-				parmSet.length);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedString });
 
-		assertTrue("The first object is not a String.",
-				parmSet[0] instanceof String);
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		// Verify the parameter
-		assertEquals("The String does not equal the expected String.",
-				expectedString, parmSet[0]);		
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
 	}
-	
+
 	@Test
 	public void parseInteger() throws Exception {
 		Integer expectedInteger = 5;
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
-			 + "<tests>"
-			 + "<test>"
-			 + "<int id=\"testInteger\" value=\""+ expectedInteger + "\" />"
-			 + "</test>"
-			 + "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
-		//Run the test
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
-		
-		//Verify the correct object structure was created
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+				+ "<tests>" + "<test>" + "<int id=\"testInteger\" value=\""
+				+ expectedInteger + "\" />" + "</test>" + "</tests>";
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of objects were not created.", 1,
-				parmSet.length);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedInteger });
 
-		assertTrue("The first object is not an Integer.",
-				parmSet[0] instanceof Integer);
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		// Verify the parameter
-		assertEquals("The Integer does not equal the expected Integer.",
-				expectedInteger, parmSet[0]);		
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
 	}
-	
+
 	@Test
 	public void parseShort() throws Exception {
 		Short expectedShort = 5;
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
-			 + "<tests>"
-			 + "<test>"
-			 + "<short id=\"testShort\" value=\""+ expectedShort + "\" />"
-			 + "</test>"
-			 + "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
-		//Run the test
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
-		
-		//Verify the correct object structure was created
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+				+ "<tests>" + "<test>" + "<short id=\"testShort\" value=\""
+				+ expectedShort + "\" />" + "</test>" + "</tests>";
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of objects were not created.", 1,
-				parmSet.length);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedShort });
 
-		assertTrue("The first object is not a Short.",
-				parmSet[0] instanceof Short);
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		// Verify the parameter
-		assertEquals("The Short does not equal the expected Short.",
-				expectedShort, parmSet[0]);		
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
 	}
-	
+
 	@Test
 	public void parseLong() throws Exception {
 		Long expectedLong = 5L;
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
-			 + "<tests>"
-			 + "<test>"
-			 + "<long id=\"testLong\" value=\""+ expectedLong + "\" />"
-			 + "</test>"
-			 + "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
-		//Run the test
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
-		
-		//Verify the correct object structure was created
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+				+ "<tests>" + "<test>" + "<long id=\"testLong\" value=\""
+				+ expectedLong + "\" />" + "</test>" + "</tests>";
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of objects were not created.", 1,
-				parmSet.length);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedLong });
 
-		assertTrue("The first object is not a Long.",
-				parmSet[0] instanceof Long);
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		// Verify the parameter
-		assertEquals("The Long does not equal the expected Long.",
-				expectedLong, parmSet[0]);		
-	}	
-	
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
+	}
+
 	@Test
 	public void parseBoolean() throws Exception {
 		Boolean expectedBoolean = false;
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
-			 + "<tests>"
-			 + "<test>"
-			 + "<boolean id=\"testString\" value=\""+ expectedBoolean + "\" />"
-			 + "</test>"
-			 + "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
-		//Run the test
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
-		
-		//Verify the correct object structure was created
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+				+ "<tests>" + "<test>" + "<boolean id=\"testString\" value=\""
+				+ expectedBoolean + "\" />" + "</test>" + "</tests>";
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of objects were not created.", 1,
-				parmSet.length);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedBoolean });
 
-		assertTrue("The first object is not a Boolean.",
-				parmSet[0] instanceof Boolean);
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		// Verify the parameter
-		assertEquals("The Boolean does not equal the expected Boolean.",
-				expectedBoolean, parmSet[0]);		
-	}	
-	
-		
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
+	}
+
 	@Test
 	public void parseBeanWithList() throws Exception {
-		//Create the "input" XML
+		// Create the "input" XML
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
 				+ "<tests>"
 				+ "<test>"
@@ -206,14 +144,11 @@ public class BasicDigesterParameterFactoryTest {
 				+ "<property name=\"id\" value=\"5\" />"
 				+ "<property name=\"model\" value=\"SAINT\" />"
 				+ "<property name=\"manufacturer\" value=\"Nova Laboratories\" />"
-				+ "</bean>" 
-				+ "</list>" 
-				+ "</property>" 
-				+ "</bean>" 
-				+ "</test>"
+				+ "</bean>" + "</list>" + "</property>" + "</bean>" + "</test>"
 				+ "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
+
 		// Create the expected parameter set
 		Robot expectedRobot = new Robot();
 		expectedRobot.setName("Daneel Olivaw");
@@ -231,46 +166,32 @@ public class BasicDigesterParameterFactoryTest {
 		expectedFriends.add(friend);
 		expectedRobot.setFriends(expectedFriends);
 
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedRobot });
 
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of beans were not created.", 1,
-				parmSet.length);
-
-		assertTrue("The first bean is not a Robot.",
-				parmSet[0] instanceof Robot);
-
-		// Verify the robot
-		assertEquals("The robot does not equal the expected robot.",
-				expectedRobot.toString(), parmSet[0].toString());
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
 	}
-	
+
 	@Test
 	public void parseBeanWithAListOfLists() throws Exception {
-		//Create the "input" XML
+		// Create the "input" XML
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
 				+ "<tests>"
 				+ "<test>"
 				+ "<bean id=\"expectedRobot\" class=\"org.junitext.runners.Robot\" >"
 				+ "<property name=\"name\" value=\"Daneel Olivaw\" />"
-				+ "<property name=\"listsOfFriends\">"
-				+ "<list>"
-				+ "<list>"				
+				+ "<property name=\"listsOfFriends\">" + "<list>" + "<list>"
 				+ "<bean id=\"friend\" class=\"org.junitext.runners.Robot\" >"
-				+ "<property name=\"name\" value=\"Johnny 5\" />"
-				+ "</bean>"
-				+ "</list>"				
-				+ "</list>" 
-				+ "</property>" 
-				+ "</bean>" 
-				+ "</test>"
+				+ "<property name=\"name\" value=\"Johnny 5\" />" + "</bean>"
+				+ "</list>" + "</list>" + "</property>" + "</bean>" + "</test>"
 				+ "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
+
 		// Create the expected parameter set
 		Robot expectedRobot = new Robot();
 		expectedRobot.setName("Daneel Olivaw");
@@ -284,49 +205,33 @@ public class BasicDigesterParameterFactoryTest {
 		listOfFriends.add(expectedFriends);
 		expectedRobot.setListsOfFriends(listOfFriends);
 
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedRobot });
 
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of beans were not created.", 1,
-				parmSet.length);
-
-		assertTrue("The first bean is not a Robot.",
-				parmSet[0] instanceof Robot);
-
-		// Verify the robot
-		assertEquals("The robot does not equal the expected robot.",
-				expectedRobot.toString(), parmSet[0].toString());
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
 	}
-	
-	
+
 	@Test
 	public void parseBeanWithAListOfListsOfLists() throws Exception {
-		//Create the "input" XML
+		// Create the "input" XML
 		String inputString = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>"
 				+ "<tests>"
 				+ "<test>"
 				+ "<bean id=\"expectedRobot\" class=\"org.junitext.runners.Robot\" >"
 				+ "<property name=\"name\" value=\"Daneel Olivaw\" />"
-				+ "<property name=\"threeLevelListOfFriends\">"
-				+ "<list>"
-				+ "<list>"
-				+ "<list>"
+				+ "<property name=\"threeLevelListOfFriends\">" + "<list>"
+				+ "<list>" + "<list>"
 				+ "<bean id=\"friend\" class=\"org.junitext.runners.Robot\" >"
-				+ "<property name=\"name\" value=\"Johnny 5\" />"
-				+ "</bean>"
-				+ "</list>"				
-				+ "</list>"				
-				+ "</list>" 
-				+ "</property>" 
-				+ "</bean>" 
-				+ "</test>"
-				+ "</tests>";
-		InputStream inputXml = new ByteArrayInputStream(inputString.getBytes("UTF-8"));
-		
+				+ "<property name=\"name\" value=\"Johnny 5\" />" + "</bean>"
+				+ "</list>" + "</list>" + "</list>" + "</property>" + "</bean>"
+				+ "</test>" + "</tests>";
+		InputStream inputXml = new ByteArrayInputStream(inputString
+				.getBytes("UTF-8"));
+
 		// Create the expected parameter set
 		Robot expectedRobot = new Robot();
 		expectedRobot.setName("Daneel Olivaw");
@@ -338,29 +243,54 @@ public class BasicDigesterParameterFactoryTest {
 		expectedFriends.add(friend);
 		List<List<Robot>> listOfFriends = new ArrayList<List<Robot>>();
 		listOfFriends.add(expectedFriends);
-		
+
 		List<List<List<Robot>>> thirdLevelList = new ArrayList<List<List<Robot>>>();
 		thirdLevelList.add(listOfFriends);
-		
+
 		expectedRobot.setThreeLevelListOfFriends(thirdLevelList);
 
-		List<Object[]> parmSets = testFactory.createParameters(inputXml);
+		List<Object[]> expectedParamSets = new ArrayList<Object[]>();
+		expectedParamSets.add(new Object[] { expectedRobot });
 
-		// Verify that the correct object structure was created.
-		assertEquals("The correct number of paramter sets were not created.",
-				1, parmSets.size());
+		// Run the test
+		List<Object[]> actualParamSets = testFactory.createParameters(inputXml);
 
-		Object[] parmSet = parmSets.get(0);
-		assertEquals("The correct number of beans were not created.", 1,
-				parmSet.length);
-
-		assertTrue("The first bean is not a Robot.",
-				parmSet[0] instanceof Robot);
-
-		// Verify the robot
-		assertEquals("The robot does not equal the expected robot.",
-				expectedRobot.toString(), parmSet[0].toString());
+		// Verify the expected outcome
+		assertParameterSetsEqual(expectedParamSets, actualParamSets);
 	}
-	
-	
+
+	/**
+	 * Asserts that two lists of parameter sets are equal. Individual objects in
+	 * a given parameter set are compaired using the <code>toString()</code>
+	 * method.
+	 * 
+	 * @param expectedParamSets
+	 * @param acutalParamSets
+	 */
+	private void assertParameterSetsEqual(List<Object[]> expectedParamSets,
+			List<Object[]> acutalParamSets) {
+		assertEquals("The correct number of paramter sets were not created.",
+				expectedParamSets.size(), acutalParamSets.size());
+
+		for (int j = 0; j < expectedParamSets.size(); j++) {
+			Object[] expectedParamSet = expectedParamSets.get(j);
+			Object[] actualParamSet = acutalParamSets.get(j);
+
+			assertEquals("The correct number of objects were not created.",
+					expectedParamSet.length, actualParamSet.length);
+
+			for (int k = 0; k < expectedParamSet.length; k++) {
+				assertEquals(
+						"The actual parameter object is not of the expected type.",
+						expectedParamSet[k].getClass(), actualParamSet[k]
+								.getClass());
+				assertEquals(
+						"The actual parameter object does not equal the expected parameter object.",
+						expectedParamSet[k].toString(), actualParamSet[k]
+								.toString());
+			}
+
+		}
+	}
+
 }
