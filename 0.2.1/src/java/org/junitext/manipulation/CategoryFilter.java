caf460e399d8b260d5d4e58604ca5c0763bb6d14
jedijim <jedijim@411c844f-e91d-0410-aa1e-e186e184c3e4>
2007-02-22 02:35:30 +0000

    Refactored Repository Structure

---
+++ 0.2.1/src/java/org/junitext/manipulation/CategoryFilter.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2006-2007, Jochen Hiller.
+ */
+package org.junitext.manipulation;
+
+import org.junit.runner.Description;
+import org.junit.runner.manipulation.Filter;
+import org.junitext.Category;
+
+/**
+ * A filter to support categories. Will run tests matching a given category.
+ */
+public class CategoryFilter extends Filter {
+
+	private String category;
+
+	public CategoryFilter(String c) {
+		this.category = c;
+	}
+
+	@Override
+	public boolean shouldRun(Description description) {
+		Category c = CategoryResolver.getCategory(description);
+		if (c != null) {
+			return c.value().equals(category);
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public String describe() {
+		return "tests for category " + String.valueOf(category);
+	}
+}
