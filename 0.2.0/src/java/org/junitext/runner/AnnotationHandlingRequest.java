e54aa724f6db81dbd36e82cde7784d2cf8260b7c
jedijim <jedijim@411c844f-e91d-0410-aa1e-e186e184c3e4>
2007-02-22 02:35:23 +0000

    Refactored Repository Structure

---
+++ 0.2.0/src/java/org/junitext/runner/AnnotationHandlingRequest.java
@@ -0,0 +1,29 @@
+package org.junitext.runner;
+
+import org.junit.internal.runners.InitializationError;
+import org.junit.runner.Request;
+import org.junit.runner.Runner;
+import org.junit.runner.manipulation.Filter;
+import org.junitext.runners.AnnotationRunner;
+
+/**
+ * Request which supports annotation handling of test cases.
+ */
+public class AnnotationHandlingRequest {
+
+	public static Request aClass(final Class<?> clazz) {
+		return new Request() {
+
+			@Override
+			public Runner getRunner() {
+				try {
+					return new AnnotationRunner(clazz);
+				} catch (InitializationError e) {
+					return Request.errorReport(Filter.class, new Exception(String
+							.format("Error creating AnnotationHandler for class %s ", clazz.getName()
+									))).getRunner();
+				}
+			}
+		};
+	}
+}
