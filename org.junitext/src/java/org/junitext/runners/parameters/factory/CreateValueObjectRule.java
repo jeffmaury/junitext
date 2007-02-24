680a769e917b3a9c1f8d343947a6799d3a9e04a9
jim.hurne <jim.hurne@411c844f-e91d-0410-aa1e-e186e184c3e4>
2007-02-24 20:04:07 +0000

    Updated the samples and the tutorial to use the new value object syntax for XML paramterized tests.

--- org.junitext/src/java/org/junitext/runners/parameters/factory/CreateValueObjectRule.java
+++ org.junitext/src/java/org/junitext/runners/parameters/factory/CreateValueObjectRule.java
@@ -17,50 +17,50 @@ import org.xml.sax.Attributes;
 
 /**
  * Digester rule which creates a "value" object based on the given type in a
- * named attribute.  The body text of the matching element is used to initialize
+ * named attribute. The body text of the matching element is used to initialize
  * the value object.
  * <p>
  * If the type attribute is not specified by the element, then it is assumed
  * that the type of the value object is <code>String</code>.
- * </p>  
+ * </p>
  * <p>
- * When the end tag is reached, the created object will be
- * popped. Note that this means that if you are going to do anything with value objects
- * created with this rule, it has to be done after this rules body text call is
- * made and BEFORE this rule's end method is called.  Thus the order in which
- * the rules are defined is very important for this rule to be effective. 
+ * When the end tag is reached, the created object will be popped. Note that
+ * this means that if you are going to do anything with value objects created
+ * with this rule, it has to be done after this rules body text call is made and
+ * BEFORE this rule's end method is called. Thus the order in which the rules
+ * are defined is very important for this rule to be effective.
  * </p>
  * 
  * @author Jim Hurne
  * 
  */
 public class CreateValueObjectRule extends Rule {
-	
+
 	private static final String VALUE_OBJECT_TYPE_STACK = "org.junitext.runners.parameters.factory."
-		+ "CreateValueObjectRule.valueObjectType";
-	
+			+ "CreateValueObjectRule.valueObjectType";
 
 	private final String typeAttributeName;
-	
+
 	/**
-	 * Creates a new <code>CreateValueObjectRule</code> with the given
-	 * type attribute name.
+	 * Creates a new <code>CreateValueObjectRule</code> with the given type
+	 * attribute name.
 	 * 
-	 * @param typeAttributeName the name of the attribute to get the value 
-	 * objects type.
+	 * @param typeAttributeName
+	 *            the name of the attribute to get the value objects type.
 	 */
 	public CreateValueObjectRule(String typeAttributeName) {
 		this.typeAttributeName = typeAttributeName;
 	}
-	
+
 	/**
-	 * @see org.apache.commons.digester.Rule#begin(java.lang.String, java.lang.String, org.xml.sax.Attributes)
+	 * @see org.apache.commons.digester.Rule#begin(java.lang.String,
+	 *      java.lang.String, org.xml.sax.Attributes)
 	 */
 	@Override
 	public void begin(String namespace, String elementName,
 			Attributes attributes) throws Exception {
-		
-		//Look to see if the type of the value object has been defined
+
+		// Look to see if the type of the value object has been defined
 		String valueType = null;
 		for (int j = 0; j < attributes.getLength(); j++) {
 			String attrName = attributes.getLocalName(j);
@@ -73,25 +73,28 @@ public class CreateValueObjectRule extends Rule {
 				valueType = attributes.getValue(j);
 			}
 		}
-		
-		if(valueType == null) {
-			//The value type was not specified, so default to String
+
+		if (valueType == null) {
+			// The value type was not specified, so default to String
 			valueType = String.class.getName();
 		}
-		
-		//Push the value type onto the value type stack
+
+		// Push the value type onto the value type stack
 		digester.push(VALUE_OBJECT_TYPE_STACK, valueType);
 	}
 
 	/**
-	 * @see org.apache.commons.digester.Rule#body(java.lang.String, java.lang.String, java.lang.String)
+	 * @see org.apache.commons.digester.Rule#body(java.lang.String,
+	 *      java.lang.String, java.lang.String)
 	 */
 	@Override
-	public void body(String namespace, String name, String bodyText) throws Exception {
-		//Get defined value type, and the class object representing the value type
+	public void body(String namespace, String name, String bodyText)
+			throws Exception {
+		// Get defined value type, and the class object representing the value
+		// type
 		String valueType = (String) digester.pop(VALUE_OBJECT_TYPE_STACK);
 		Class<?> valueTypeClass = Class.forName(valueType);
-		
+
 		if (digester.getLogger().isDebugEnabled()) {
 			StringBuilder logMessage = new StringBuilder();
 			logMessage.append("[CreateValueObjectRule]{");
@@ -103,24 +106,27 @@ public class CreateValueObjectRule extends Rule {
 			logMessage.append("]");
 			digester.getLogger().debug(logMessage.toString());
 		}
-		
-		//Create the value object using the body text performing the necessary conversion
+
+		// Create the value object using the body text performing the necessary
+		// conversion
 		Object valueObject = ConvertUtils.convert(bodyText, valueTypeClass);
 		digester.push(valueObject);
 	}
 
 	/**
-	 * @see org.apache.commons.digester.Rule#end(java.lang.String, java.lang.String)
+	 * @see org.apache.commons.digester.Rule#end(java.lang.String,
+	 *      java.lang.String)
 	 */
 	@Override
 	public void end(String namespace, String name) throws Exception {
-		//Pop the value object off the stack.
+		// Pop the value object off the stack.
 		if (digester.getLogger().isDebugEnabled()) {
-			digester.getLogger().debug("[CreateValueObjectRule] Popping value object");
-		}		
+			digester.getLogger().debug(
+					"[CreateValueObjectRule] Popping value object");
+		}
 		digester.pop();
 	}
-	
+
 	/**
 	 * @see java.lang.Object#toString()
 	 */
@@ -131,5 +137,5 @@ public class CreateValueObjectRule extends Rule {
 		sb.append(typeAttributeName);
 		sb.append("]");
 		return sb.toString();
-	}	
+	}
 }
